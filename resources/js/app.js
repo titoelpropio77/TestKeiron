0e5d8d074fbd51322078af422ee5048508211d79
Joan Andrés Lara Mora <joandres52752lm@gmail.com>
2020-04-11 20:41:24 -0500

    [Javascript, Blade] Rutas para los componentes, arreglada.

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,5 +1,8 @@
 import { render } from 'react-dom';
+import * as ReactRouterDOM from 'react-router-dom';
+import React from 'react';
 import App from './components/App';
+
 /**
  * First we will load all of this project's JavaScript dependencies which
  * includes React and other helpers. It's a great starting point while
@@ -13,4 +16,9 @@ require('./bootstrap');
  * the page. Then, you may begin adding components to this application
  * or customize the JavaScript scaffolding to fit your unique needs.
  */
-render(<App/>, document.getElementById('react-login'));
+
+render((
+ <ReactRouterDOM.BrowserRouter>
+  <App/>
+ </ReactRouterDOM.BrowserRouter>
+), document.getElementById('react-login'));
